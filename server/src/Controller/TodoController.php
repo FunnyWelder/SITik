15513fd0d0965df0484de3a225c446751512fe46
FunnyWelder <slavakuzm@mail.ru>
2021-12-13 20:11:54 +1000

    Add TodoController

---
+++ server/src/Controller/TodoController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Todo;
+use App\Previewer\TodoPreviewer;
+use App\Repository\TodoRepository;
+use App\Repository\UserRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/todo', name: 'todo_')]
+class TodoController extends ApiController
+{
+    private EntityManagerInterface $em;
+    private TodoRepository $todoRepository;
+    private UserRepository $userRepository;
+
+    public function __construct(
+        EntityManagerInterface $em,
+        TodoRepository $todoRepository,
+        UserRepository $userRepository,
+    ){
+        $this->em = $em;
+        $this->todoRepository = $todoRepository;
+        $this->userRepository = $userRepository;
+    }
+
+    #[Route(name: 'show_self_all', methods: ['GET'])]
+    public function showSelfAll(TodoPreviewer $todoPreviewer): JsonResponse
+    {
+        $user = $this->userRepository->findOneBy(['username'=>$this->getUser()->getUserIdentifier()]);
+        $todos = $this->todoRepository->findBy(['author' => $user]);
+
+        $arrayTodo = [];
+        foreach ($todos as $todo) {
+            $arrayTodo[] = $todoPreviewer->preview($todo);
+        }
+
+        return $this->response($arrayTodo);
+    }
+
+    #[Route(name: 'new', methods: ['POST'])]
+    public function new(Request $request): JsonResponse
+    {
+        $request = $request->request->all();
+        $todo = new todo();
+
+        $author = $this->userRepository->find($request['author_id']);
+        if (!$author) {
+            return $this->respondNotFound("Author not found");
+        }
+
+        try {
+            $todo
+                ->setDescription($request['description'])
+                ->setDone(false)
+                ->setDateCreated(new DateTime())
+                ->setAuthor($author);
+
+            $this->em->persist($todo);
+            $this->em->flush();
+
+            return $this->respondWithSuccess("Todo added successfully");
+        } catch (Exception) {
+            return $this->respondValidationError();
+        }
+    }
+
+    #[Route('/{todo_id}', name: 'show', requirements: ['todo_id' => '\d+'], methods: ['GET'])]
+    public function show(TodoPreviewer $todoPreviewer, $todo_id): JsonResponse
+    {
+        $todo = $this->todoRepository->find($todo_id);
+        if (!$todo) {
+            return $this->respondNotFound("Todo not found");
+        }
+
+        return $this->response($todoPreviewer->preview($todo));
+    }
+
+    #[Route('/{todo_id}', name: 'edit', requirements: ['todo_id' => '\d+'], methods: ['PUT'])]
+    public function edit(Request $request, $todo_id): JsonResponse
+    {
+        $todo = $this->todoRepository->find($todo_id);
+        if (!$todo) {
+            return $this->respondNotFound("Todo not found");
+        }
+
+        $request = $request->request->all();
+
+        try {
+            if (isset($request['description'])) {
+                $todo->setDescription($request['description']);
+            }
+            if (isset($request['done'])) {
+                $todo->setDone($request['done']);
+            }
+            if (isset($request['author_id'])) {
+                $author = $this->userRepository->find($request['author_id']);
+                if (!$author) {
+                    return $this->respondNotFound("Author not found");
+                }
+                $todo->setAuthor($author);
+            }
+
+            $this->em->flush();
+
+            return $this->respondWithSuccess("Todo updated successfully");
+        } catch (Exception) {
+            return $this->respondValidationError();
+        }
+    }
+
+    #[Route('/{todo_id}', name: 'delete', requirements: ['todo_id' => '\d+'], methods: ['DELETE'])]
+    public function delete($todo_id): JsonResponse
+    {
+        $todo = $this->todoRepository->find($todo_id);
+        if (!$todo) {
+            return $this->respondNotFound("Todo not found");
+        }
+
+        $this->em->remove($todo);
+        $this->em->flush();
+
+        return $this->respondWithSuccess("Todo deleted successfully");
+    }
+}
---
+++ server/src/Previewer/TodoPreviewer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Previewer;
+
+use App\Entity\Todo;
+
+class TodoPreviewer
+{
+    private UserPreviewer $userPreviewer;
+
+    public function __construct(
+        UserPreviewer $userPreviewer
+    ){
+        $this->userPreviewer = $userPreviewer;
+    }
+
+    public function preview(Todo $todo): array
+    {
+        return [
+            "id" => $todo->getId(),
+            "username" => $todo->getDateCreated(),
+            "description" => $todo->getDescription(),
+            "done" => $todo->getDone(),
+            "author" => $this->userPreviewer->preview($todo->getAuthor())
+        ];
+    }
+}
