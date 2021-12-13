910a0aa667a030ed322edb90096c685d6e74f686
FunnyWelder <slavakuzm@mail.ru>
2021-12-13 19:12:43 +1000

    fix softdelete + make migration

---
+++ server/migrations/Version20211213091155.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211213091155 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE todo (id INT AUTO_INCREMENT NOT NULL, author_id INT NOT NULL, done TINYINT(1) NOT NULL, description LONGTEXT NOT NULL, date_created DATETIME NOT NULL, deleted_at DATETIME DEFAULT NULL, INDEX IDX_5A0EB6A0F675F31B (author_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, deleted_at DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE todo ADD CONSTRAINT FK_5A0EB6A0F675F31B FOREIGN KEY (author_id) REFERENCES `user` (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE todo DROP FOREIGN KEY FK_5A0EB6A0F675F31B');
+        $this->addSql('DROP TABLE todo');
+        $this->addSql('DROP TABLE `user`');
+    }
+}
