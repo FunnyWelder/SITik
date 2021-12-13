c87924bb44051b6a418aa111a50ac93108f7858e
FunnyWelder <slavakuzm@mail.ru>
2021-12-14 02:48:10 +1000

    Залочил роуты

--- server/src/Controller/TodoController.php
+++ server/src/Controller/TodoController.php
@@ -30,20 +30,6 @@ class TodoController extends ApiController
         $this->userRepository = $userRepository;
     }
 
-    #[Route(name: 'show_self_all', methods: ['GET'])]
-    public function showSelfAll(TodoPreviewer $todoPreviewer): JsonResponse
-    {
-        $user = $this->userRepository->findOneBy(['username'=>$this->getUser()->getUserIdentifier()]);
-        $todos = $this->todoRepository->findBy(['author' => $user]);
-
-        $arrayTodo = [];
-        foreach ($todos as $todo) {
-            $arrayTodo[] = $todoPreviewer->preview($todo);
-        }
-
-        return $this->response($arrayTodo);
-    }
-
     #[Route(name: 'new', methods: ['POST'])]
     public function new(Request $request): JsonResponse
     {
@@ -119,6 +105,86 @@ class TodoController extends ApiController
     public function delete($todo_id): JsonResponse
     {
         $todo = $this->todoRepository->find($todo_id);
+        if (!$todo) {
+            return $this->respondNotFound("Todo not found");
+        }
+
+        $this->em->remove($todo);
+        $this->em->flush();
+
+        return $this->respondWithSuccess("Todo deleted successfully");
+    }
+
+    #[Route('/self', name: 'show_self_all', methods: ['GET'])]
+    public function showSelfAll(TodoPreviewer $todoPreviewer): JsonResponse
+    {
+        $user = $this->userRepository->findOneBy(['username'=>$this->getUser()->getUserIdentifier()]);
+        $todos = $this->todoRepository->findBy(['author' => $user]);
+
+        $arrayTodo = [];
+        foreach ($todos as $todo) {
+            $arrayTodo[] = $todoPreviewer->preview($todo);
+        }
+
+        return $this->response($arrayTodo);
+    }
+
+    #[Route('/self', name: 'new_self', methods: ['POST'])]
+    public function newSelf(Request $request): JsonResponse
+    {
+        $author = $this->userRepository->findOneBy(['username'=>$this->getUser()->getUserIdentifier()]);
+
+        $request = $request->request->all();
+        $todo = new todo();
+
+        try {
+            $todo
+                ->setDescription($request['description'])
+                ->setDone(false)
+                ->setDateCreated(new DateTime())
+                ->setAuthor($author);
+
+            $this->em->persist($todo);
+            $this->em->flush();
+
+            return $this->respondWithSuccess("Todo added successfully");
+        } catch (Exception) {
+            return $this->respondValidationError();
+        }
+    }
+
+    #[Route('/{todo_id}', name: 'edit_self', requirements: ['todo_id' => '\d+'], methods: ['PUT'])]
+    public function editSelf(Request $request, $todo_id): JsonResponse
+    {
+        $author = $this->userRepository->findOneBy(['username'=>$this->getUser()->getUserIdentifier()]);
+        $todo = $this->todoRepository->findOneBy(['author'=>$author, 'id'=>$todo_id]);
+        if (!$todo) {
+            return $this->respondNotFound("Todo not found");
+        }
+
+        $request = $request->request->all();
+
+        try {
+            if (isset($request['description'])) {
+                $todo->setDescription($request['description']);
+            }
+            if (isset($request['done'])) {
+                $todo->setDone($request['done']);
+            }
+
+            $this->em->flush();
+
+            return $this->respondWithSuccess("Todo updated successfully");
+        } catch (Exception) {
+            return $this->respondValidationError();
+        }
+    }
+
+    #[Route('/self/{todo_id}', name: 'delete_self', requirements: ['todo_id' => '\d+'], methods: ['DELETE'])]
+    public function deleteSelf($todo_id): JsonResponse
+    {
+        $author = $this->userRepository->findOneBy(['username'=>$this->getUser()->getUserIdentifier()]);
+        $todo = $this->todoRepository->findOneBy(['author'=>$author, 'id'=>$todo_id]);
         if (!$todo) {
             return $this->respondNotFound("Todo not found");
         }
